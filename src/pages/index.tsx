3438ce2e94b9e1fef172656ecc648b8adcfc8e1a
Gustavo-NSAbreu <guustavoabreu99@gmail.com>
2022-10-28 12:00:46 -0300

    Added random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,19 @@
+import { getOptionForVote } from '@/utils/getRandomPokemon'
 import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
 
-  const {data, isLoading} = trpc.useQuery(["hello", { text: "Gustavo" }]);
-
-  if (isLoading) return <div>Loading...</div>
-  
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionForVote();
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which pokemon is Rounder?</div>
       <div className='p-2' /> 
       <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-        <div className='w-16 h-16 bg-red-200'/>
+        <div className='w-16 h-16 bg-red-800'>{first}</div>
         <div className='p-8'>Vs</div>
-        <div className='w-16 h-16 bg-red-200'/>
+        <div className='w-16 h-16 bg-red-800'>{second}</div>
       </div>
     </div>
   )
